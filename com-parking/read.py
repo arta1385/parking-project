735cf3bae3ec9c46b4f22bc77ce1c463ea86cf40
arta1385 <74140240+arta1385@users.noreply.github.com>
2021-12-07 19:42:20 +0330

    car detection

--- com-parking/read.py
+++ com-parking/read.py
@@ -1,6 +1,9 @@
 import cv2
-img=cv2.imread('car.jpg')
-imgblur=cv2.blur(img,(3,3))
-cv2.imshow('car',img)
-cv2.imshow('carblur',imgblur)
+car_cascade=cv2.CascadeClassifier('cars.xml')
+img=cv2.imread('car3.jpg')
+imggray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cars=car_cascade.detectMultiScale(imggray,1.1,4)
+for (x,y,w,h) in cars:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+cv2.imshow('img',img)
 cv2.waitKey(0)
